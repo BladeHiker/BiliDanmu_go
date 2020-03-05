5e3131f3a54eee30f2cfa3ba2bf5a801c2aba547
shiluoCoder <690898835@qq.com>
2020-03-05 15:08:58 +0800

    🔧 compatible with short RoomID

--- main.go
+++ main.go
@@ -10,12 +10,22 @@ import (
 
 func main() {
 	var roomid uint32
-	fmt.Print("请输入房间号，目前仅支持长 ID：")
+	fmt.Print("请输入房间号，长短 ID 均可：")
 	_, err := fmt.Scanf("%d", &roomid)
 	if err != nil {
 		log.Println("房间号输入错误，请退出重新输入！")
 		os.Exit(0)
 	}
+
+	// 兼容房间号短 ID
+	if roomid >= 100 && roomid < 1000 {
+		roomid,err = models.GetRealRoomID(int(roomid))
+		if err != nil {
+			log.Println("房间号输入错误，请退出重新输入！")
+			os.Exit(0)
+		}
+	}
+
 	c, err := models.NewClient(roomid)
 	if err != nil {
 		fmt.Println("models.NewClient err: ", err)
