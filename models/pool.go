197f173268881416130241eb9a855352a27c888c
ciisaichan <chen_studio@qq.com>
2021-06-07 17:31:08 +0800

    修复舰长欢迎信息导致信息卡死，优化指针传递

--- models/pool.go
+++ models/pool.go
@@ -2,24 +2,27 @@ package models
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Pool`s fields map CMD value
 type Pool struct {
-	UserMsg         chan string
-	UserGift        chan string
-	UserEnter       chan string
-	UserGuard       chan string
-	MsgUncompressed chan string
-	UserEntry       chan string
+	UserMsg         chan []byte
+	UserGift        chan []byte
+	UserEnter       chan []byte
+	UserGuard       chan []byte
+	MsgUncompressed chan []byte
+	UserEntry       chan []byte
 }
 
 func NewPool() *Pool {
 	return &Pool{
-		UserMsg:         make(chan string, 10),
-		UserGift:        make(chan string, 10),
-		UserEnter:       make(chan string, 10),
-		MsgUncompressed: make(chan string, 10),
+		UserMsg:         make(chan []byte, 10),
+		UserGift:        make(chan []byte, 10),
+		UserEnter:       make(chan []byte, 10),
+		MsgUncompressed: make(chan []byte, 10),
+		UserEntry:       make(chan []byte, 10),
+		UserGuard:       make(chan []byte, 10),
 	}
 }
 
@@ -28,27 +31,29 @@ func (pool *Pool) Handle() {
 		select {
 		case uc := <-pool.MsgUncompressed:
 			// 目前只处理未压缩数据的关注数变化信息
-			if cmd := json.Get([]byte(uc), "cmd").ToString(); CMD(cmd) == CMDRoomRealTimeMessageUpdate {
-				fans := json.Get([]byte(uc), "data", "fans").ToInt()
+			if cmd := json.Get(uc, "cmd").ToString(); CMD(cmd) == CMDRoomRealTimeMessageUpdate {
+				fans := json.Get(uc, "data", "fans").ToInt()
 				fmt.Println("当前房间关注数变动：", fans)
 			}
 		case src := <-pool.UserMsg:
 			m := NewDanmu()
-			m.GetDanmuMsg([]byte(src))
+			m.GetDanmuMsg(src)
 			fmt.Printf("%d-%s | %d-%s: %s\n", m.MedalLevel, m.MedalName, m.Ulevel, m.Uname, m.Text)
 		case src := <-pool.UserGift:
 			g := NewGift()
-			g.GetGiftMsg([]byte(src))
+			g.GetGiftMsg(src)
 			fmt.Printf("%s %s 价值 %d 的 %s\n", g.UUname, g.Action, g.Price, g.GiftName)
 		case src := <-pool.UserEnter:
-			name := json.Get([]byte(src), "data", "uname").ToString()
-			fmt.Printf("欢迎VIP %s 进入直播间", name)
+			name := json.Get(src, "data", "uname").ToString()
+			fmt.Printf("欢迎VIP %s 进入直播间\n", name)
 		case src := <-pool.UserGuard:
-			name := json.Get([]byte(src), "data", "username").ToString()
-			fmt.Printf("欢迎房管 %s 进入直播间", name)
+			name := json.Get(src, "data", "username").ToString()
+			fmt.Printf("欢迎房管 %s 进入直播间\n", name)
 		case src := <-pool.UserEntry:
-			cw := json.Get([]byte(src), "data", "copy_writing").ToString()
-			fmt.Printf("%s", cw)
+			cw := json.Get(src, "data", "copy_writing").ToString()
+			cw = strings.Replace(cw, "<%", "", 1)
+			cw = strings.Replace(cw, "%>", "", 1)
+			fmt.Printf("%s\n", cw)
 		}
 	}
 }
